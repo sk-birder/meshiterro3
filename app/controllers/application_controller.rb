7ad6f795124b273a76eb490da45e0acece11dfab
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-03-25 12:35:06 +0000

    [Finish] App2-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # サインアップ・サインイン後の遷移先をAboutページに変更
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  # サインアウト後の遷移先をAboutページに変更
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+  
   protected
 
   def configure_permitted_parameters
