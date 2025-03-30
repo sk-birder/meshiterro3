8cef86d738a955d81149a94685c7268249e8bdb4
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-03-30 13:14:52 +0000

    [Finish] Application2-19 typo修正のみ

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,6 @@
 class FavoritesController < ApplicationController
   def create
-    post_image = PostImage.find(parms[:post_image_id])
+    post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.new(post_image_id: post_image.id) # 省略形？(19章)
     favorite.save
     redirect_to post_image_path(post_image)
@@ -10,6 +10,6 @@ class FavoritesController < ApplicationController
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: post_image.id) # 省略形？(19章)
     favorite.destroy
-    redirect_to post_image_path(@ost_image)
+    redirect_to post_image_path(post_image)
   end
 end
