a01e77f705ce5b610ec2ab8a1d583b2a87cc8cbb
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-04-07 02:04:36 +0000

    [Finish] Application2-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :upadate]
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -18,4 +20,13 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+  # editとupdate用のユーザー判定メソッド
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path # post_images#indexへリダイレクト
+    end
+  end
+
 end
